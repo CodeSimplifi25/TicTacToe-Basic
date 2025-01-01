527493ccce4bc40a310d2925af36a075612c37ec
codeSimplifi <reham.ttm@gmail.com>
2025-01-01 13:06:17 -0500

    adds pseudocode

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize the board
+// Set current player to 'X'
+// Set gameOver to false
+// Set the winning combinations
+
+// Game Loop - repeat until game is over
+    // Player picks an empty square
+    // Check for a winner
+    // Check for a tie
+    // Switch Player
